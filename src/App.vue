eb65a7e0c2bb139429d623f0dc67abe5c4073057
SamanSah <sahraei.saman91@gmail.com>
2023-05-16 19:47:19 +0330

    init pomodoro app with vuetify

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col sm="6" offset-sm="3">
+            <pompdoro-app />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+</v-app>
+  
+</template>
+
+<script>
+import Pomodoro from './components/Pomodoro.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'pompdoro-app': Pomodoro
+  }
+}
+</script>
+
+<style>
+.v-application__wrap {
+  max-height: 100%;
+}
+</style>
---
+++ src/components/Pomodoro.vue
@@ -0,0 +1,120 @@
+<template>
+  <v-card color="basil">
+    <v-card-title class="text-center justify-center py-6">
+      <h1 class="font-weight-bold text-h4 text-basil">
+        POMODORO
+      </h1>
+    </v-card-title>
+
+    <v-tabs
+      v-model="tab"
+      bg-color="transparent"
+      color="basil"
+      grow
+    >
+      <v-tab
+        v-for="tab in tabTitles"
+        :key="tab"
+        :value="tab"
+      >
+        {{ tab }}
+      </v-tab>
+    </v-tabs>
+
+    <v-window v-model="tab">
+      <v-window-item
+        v-for="tab in tabTitles"
+        :key="tab"
+        :value="tab"
+      >
+        <v-card
+          color="basil"
+          flat
+        >
+          <div class="text-h1 font-weight-bold">
+            {{ displayMinutes }}:{{ displaySeconds }}
+          </div>
+        </v-card>
+        <v-container class="text-center">
+          <v-row justify="center">
+            <v-col cols="4" sm="4" md="4">
+              <v-btn @click="start()" 
+              rounded="sm" 
+              size="x-large" 
+              class="bg-deep-purple">
+                Start
+              </v-btn>
+            </v-col>
+            <v-col cols="4" sm="4" md="4">
+              <v-btn @click="stop()" 
+              rounded="sm" 
+              size="x-large" 
+              class="bg-amber">
+                Stop
+              </v-btn>
+            </v-col>
+            <v-col cols="4" sm="4" md="4">
+              <v-btn  @click="reset()"
+              rounded="sm" 
+              size="x-large" 
+              class="bg-black">
+                Reset
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-window-item>
+    </v-window>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tab: 'Pomodoro',
+      tabTitles: ['Pomodoro', 'Short Break', 'Long Break'],
+      timerInstance: null,
+      totalSecond: 25 * 60,
+      timerType: 0
+    }
+  },
+  computed: {
+    displayMinutes() {
+      let minutes= Math.floor(this.totalSecond / 60);
+      return this.formatTime(minutes)
+    },
+    displaySeconds() {
+      let seconds= this.totalSecond % 60;
+      return this.formatTime(seconds)
+    }
+  },
+  methods: {
+    formatTime(time) {
+      console.log('formatmethod');
+      console.log(time);
+      if (time < 10) {
+        return '0'+ time
+      }
+      return time.toString()
+    },
+    start() {
+      this.stop();
+      this.timerInstance = setInterval(() => {
+        this.totalSecond -=1
+      }, 1000)
+    },
+    stop() {
+      clearInterval(this.timerInstance)
+    },
+    reset() {
+      this.totalSecond= 25 * 60
+    },
+  },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
